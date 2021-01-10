55997f3a5a423da3ff02287971962974a4df39c2
Marlon Raphael <raphael27atm@gmail.com>
2021-01-10 09:46:57 -0300

    Create user guard service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { LoginComponent } from './login/login.component';
+import { FavoritesComponent } from './favorites/favorites.component';
+import { AuthGuardService } from './auth-guard.service';
 import { ProductsListComponent } from './products-list/products-list.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'products', pathMatch: 'full'},
+  { path: '', redirectTo: 'products', pathMatch: 'full' },
   { path: 'products', component: ProductsListComponent },
+  { path: 'login', component: LoginComponent },
+  {
+    path: 'favorites',
+    component: FavoritesComponent,
+    canActivate: [AuthGuardService],
+  },
 ];
 
 @NgModule({
