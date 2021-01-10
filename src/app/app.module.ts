b59aff9cdc244a1f5face3507f6ffde832f2de73
Marlon Raphael <raphael27atm@gmail.com>
2021-01-10 09:47:33 -0300

    Change app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ProductsListComponent } from './products-list/products-list.component';
+import { LoginComponent } from './login/login.component';
+import { FavoritesComponent } from './favorites/favorites.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ProductsListComponent
+    ProductsListComponent,
+    LoginComponent,
+    FavoritesComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+    HttpClientModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
